2579190bff72527245a87fcc84b1bdd3d1195324
taturou <taturou@gmail.com>
2021-03-23 16:21:12 +0900

    Added comments to the types that are to create types to communicate between the background task and the other task.

--- src/background/message/_types.ts
+++ src/background/message/_types.ts
@@ -18,15 +18,78 @@ type messageDataObjectType<R> = {
 
 type messageDataUnionType<R> = messageDataObjectType<R>[keyof messageDataObjectType<R>]
 
-// R: RepositoryType
+/*
+  Create parameters type to communicate packets between the background task and the other task.
+
+  Generics types:
+  * R: RepositoryType
+
+  If you create MutationsType as a input is:
+    ```
+    interface RepositoryType {
+      put (payload: { id: number, value: string }): Promise<void>
+      delete (payload: { id: number }): Promise<void>
+    }
+    ```
+  the output type is:
+    ```
+    type messageDataType = messageDataType<RepositoryType> = {
+      type: "put";
+      params: {
+        id: number;
+        value: string;
+      };
+      response: void;
+    } | {
+      type: "delete";
+      params: {
+        id: number;
+      };
+      response: void;
+    }
+    ```
+*/
 export type messageDataType<R> = messageDataUnionType<R>
 
-type messageDataWrapperType<O, R> = {
+type messageDataWrapperType<T, R> = {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  [P in O]: messageDataType<R>
+  [P in T]: messageDataType<R>
 }
 
-// T: Module type
-// R: RepositoryType
+/*
+  Create parameters type to communicate packets between the background task and the other task.
+
+  Generics types:
+  * T: Module name
+  * R: RepositoryType
+
+  If you create MutationsType as a input is:
+    ```
+    interface RepositoryType {
+      put (payload: { id: number, value: string }): Promise<void>
+      delete (payload: { id: number }): Promise<void>
+    }
+    ```
+  the output type is:
+    ```
+    type messageDataType = messageDataType<'boo', RepositoryType> = {
+      type: 'boo',
+      boo: {
+             type: "put";
+             params: {
+               id: number;
+               value: string;
+             };
+             response: void;
+           } | {
+             type: "delete";
+             params: {
+               id: number;
+             };
+             response: void;
+           }
+    }
+    ```
+*/
 export type messageType<T extends string, R> = messageTypeType<T> & messageDataWrapperType<T, R>
