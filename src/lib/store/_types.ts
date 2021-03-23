14a4d54da0774d33f1784a1d1cb4a162c9b314fd
taturou <taturou@gmail.com>
2021-03-23 16:16:55 +0900

    Added comments to the types that are to create types to resolve types of a Vuex store.

--- src/lib/store/_types.ts
+++ src/lib/store/_types.ts
@@ -3,9 +3,46 @@ import { Store as baseStore, CommitOptions, DispatchOptions, ActionContext } fro
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type FuncType = (...args: any) => any
 
-// R: RootState
-// S: StateType
-// M: MutationsType
+/*
+  Create an alternative parameter type for Vuex.ActionContext.
+
+  Generics types:
+  * R: RootState
+  * S: StateType
+  * M: MutationsType
+
+  This type will replace only the 'commit' property.
+  The 'commit' property will be...
+  If you create MutationType as a input is:
+    ```
+    interface MutationsType {
+      put (state: StateType, { id: number, value: string }): void
+      delete (state: StateType, { id: number }): void
+    }
+    ```
+  the output type is:
+    ```
+    type ActionsInjecteeType = StoreActionsInjecteeType<R, S, MutationsType> = {
+      commit(key: 'put', payload: { id: number, value: string}): void
+    } | {
+      commit(key: 'delete', payload: { id: number }): void
+    }
+    ```
+
+  The implement of the actions of Vuex can use parameter 'commit' like this:
+    ```
+    const actions = {
+      async put ({ commit }, payload: { id: number, value: string }): Promise<void> {
+        const { id, value } = await fooPut(id, value)
+        commit('put, { id: id, value: value })
+      },
+      async delete ({ commit }, payload: { id: number }): Promise<void> {
+        const { id } = await fooDelete(id)
+        commit('delete, { id: id })
+      }
+    }
+    ```
+*/
 export type StoreActionsInjecteeType<R, S, M> = {
   commit<
     K extends keyof M
@@ -16,11 +53,91 @@ export type StoreActionsInjecteeType<R, S, M> = {
 }
 & Omit<ActionContext<S, R>, "commit">
 
-// T: Module name
-// S: StateType
-// G: GettersType
-// M: MutationsType
-// A: ActionsType
+/*
+  Create an alternative Store type for Vuex.
+
+  Generics types:
+  * T: Module name
+  * S: StateType
+  * G: GettersType
+  * M: MutationsType
+  * A: ActionsType
+
+  This type provides 'getters', 'commit', and 'dispatch' properties that support Vuex module.
+
+  The 'getters' property will be...
+  If you create GettersType as a input is:
+    ```
+    interface GettersType {
+        get (state: StateType): string
+    }
+    ```
+  the output type is:
+    ```
+    type StoreModuleType = StoreModuleType<'boo', S, GettersType, M, A> = {
+      getters: {
+        ['boo/get']: number
+      }
+    }
+    ```
+
+  The 'commit' property will be...
+  If you create MutationsType as a input is:
+    ```
+    interface MutationsType {
+      put (state: StateType, payload: { id: number, value: string }): void
+      delete (state: StateType, payload: { id: number }): void
+    }
+    ```
+  the output type is:
+    ```
+    type StoreModuleType = StoreModuleType<'boo', S, G, MutationsType, A> = {
+      commit(
+        key: 'boo/put',
+        payload?: { id: number, value: string },
+        options?: vuex.CommitOptions
+      ): void
+    } | {
+      commit(
+        key: 'boo/delete',
+        payload?: { id: number },
+        options?: vuex.CommitOptions
+      ): void
+    }
+    ```
+
+  The 'dispatch' property will be...
+  If you create ActionsType as a input is:
+    ```
+    interface ActionsType {
+      put (injectee: ActionsInjecteeTyp, payload: { id: number, value: string }): Promise<void>
+      delete (injectee: ActionsInjecteeTyp, payload: { id: number }): Promise<void>
+    }
+    ```
+  the output type is:
+    ```
+    type StoreModuleType = StoreModuleType<'boo', S, G, M, ActionsType> = {
+      dispatch(
+        key: 'boo/put',
+        payload?: { id: number, value: string },
+        options?: vuex.DispatchOptions
+      ): Promise<void>
+    } | {
+      dispatch(
+        key: 'boo/delete',
+        payload?: { id: number },
+        options?: vuex.DispatchOptions
+      ): Promise<void>
+    }
+    ```
+
+  Users can use the store with Type prediction in Typescript when you cast the return value of Vuex.useStore() as StoreModuleype, like this:
+    ```
+    const store = vuex.useStore() as StoreModuleType<'boo', ...>;
+    const value: string = store.getters['boo/get]
+    store.dispatch('boo/put', { id: 0, value: 'foo' })
+    ```
+*/
 export type StoreModuleType<T extends string, S, G, M, A> = {
   getters: {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
